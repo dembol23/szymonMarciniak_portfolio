b6e80d345469a394b6add100b3a40d54855ccf61
dembol23 <wdembny.2005@gmail.com>
2023-05-14 23:15:56 +0200

    files formatting

--- assets/scripts/imageSlider.js
+++ assets/scripts/imageSlider.js
@@ -2,23 +2,22 @@ let slideIndex = 1;
 chooseSlide(slideIndex);
 
 function nextSlide(n) {
-    chooseSlide(slideIndex += n);
-  }
+  chooseSlide(slideIndex += n);
+}
 function currentSlide(n) {
-    chooseSlide(slideIndex = n);
-  }
+  chooseSlide(slideIndex = n);
+}
 function chooseSlide(n) {
   const slides = document.getElementsByClassName("image-slider-image-wrapper");
   const dots = document.getElementsByClassName("image-slider__pagination-dot");
-//   let slideIndex=n
   n > slides.length ? (slideIndex = 1) : "";
-  n < 1 ? (slideIndex = slides.length):'';
+  n < 1 ? (slideIndex = slides.length) : '';
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].classList.remove('active');
   }
-  slides[slideIndex-1].style.display = "flex";
-  dots[slideIndex-1].classList.add("active");
+  slides[slideIndex - 1].style.display = "flex";
+  dots[slideIndex - 1].classList.add("active");
 }
